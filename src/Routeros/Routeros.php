1073c28c7fa22ca28867c3b786712a5f591eaa16
Eugene <eugenevdm@gmail.com>
2016-11-16 16:39:18 +0200

    Added extended example file.

--- src/Routeros/Routeros.php
+++ src/Routeros/Routeros.php
@@ -4,11 +4,14 @@ namespace Routeros;
 
 class Routeros
 {
-    
+
+    public $resources;
+
     private $api;
 
     public $ip_address;
-    private $api_user, $api_password;
+
+    private $username, $password;
 
     public $command;
 
@@ -81,5 +84,38 @@ class Routeros
     public function ping($ip) {
         return Router::ping($ip);
     }
+
+    public function version() {
+        return $this->resource('version');
+    }
+
+    public function uptime() {
+        return $this->resource('uptime');
+    }
+
+    public function resource($resource)
+    {
+        if ($this->resources) {
+            return $this->resources[0][$resource];
+        }
+        $this->resources = $this->command('/system/resource/print');
+        return $this->resources[0][$resource];
+    }
+
+    private function command($command)
+    {
+        $array = [];
+        if ($this->api->connect($this->ip_address, $this->username, $this->password)) {
+            $this->api->write($command);
+            $READ = $this->api->read(false);
+            $array = $this->api->parseResponse($READ);
+            $this->api->disconnect();
+        }
+        return $array;
+    }
+
+
+
+
 
 }
